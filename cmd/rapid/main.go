2e9c52b0719244beb6b23f1344903fbaf36d73b5
0x0FACED <https.genius@gmail.com>
2025-02-18 20:21:16 +0300

    refactor: removed charm framework

--- cmd/rapid/main.go
+++ cmd/rapid/main.go
@@ -2,16 +2,12 @@ package main
 
 import (
 	"fmt"
-	"os"
 
 	"github.com/0x0FACED/rapid/configs"
-	"github.com/0x0FACED/rapid/internal/app"
 	"github.com/0x0FACED/rapid/internal/lan/client"
 	"github.com/0x0FACED/rapid/internal/lan/mdnss"
 	"github.com/0x0FACED/rapid/internal/lan/server"
 	"github.com/0x0FACED/rapid/pkg/generator"
-	"github.com/charmbracelet/bubbles/filepicker"
-	tea "github.com/charmbracelet/bubbletea"
 	"github.com/google/uuid"
 )
 
@@ -30,20 +26,7 @@ func main() {
 		return
 	}
 
-	fp := filepicker.New()
-	fp.CurrentDirectory, err = os.UserHomeDir()
-	if err != nil {
-		fmt.Println("Ошибка:", err)
-		return
-	}
-
 	c := client.New(mdnss)
 	s := server.New(configs.LANServerConfig{DownloadsDir: "./test-dir"})
-	go s.Start()
-
-	p := tea.NewProgram(app.NewApp(c, s), tea.WithAltScreen())
-	if _, err := p.Run(); err != nil {
-		fmt.Println("Ошибка:", err)
-		os.Exit(1)
-	}
+	s.Start()
 }
