82e5fb843dcff61da7c480dd5749db507b4e8b9e
0x0FACED <https.genius@gmail.com>
2025-02-23 22:00:30 +0300

    refactor: removed redundant methods

--- internal/rapid/app.go
+++ internal/rapid/app.go
@@ -2,17 +2,12 @@ package rapid
 
 import (
 	"context"
-	"fmt"
-	"strconv"
 	"sync"
-	"time"
 
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/container"
-	"fyne.io/fyne/v2/widget"
 	"github.com/0x0FACED/rapid/internal/lan/client"
 	"github.com/0x0FACED/rapid/internal/lan/server"
-	"github.com/0x0FACED/rapid/internal/model"
 	"github.com/0x0FACED/rapid/internal/rapid/controller"
 )
 
@@ -58,7 +53,7 @@ func (a *Rapid) createMainWindow() fyne.Window {
 	mainWindow.Resize(fyne.NewSize(800, 600))
 
 	content := container.NewBorder(
-		createTopPanel(),
+		a.lanController.CreateTopPanel(mainWindow),
 		createFooter(),
 		nil,
 		nil,
@@ -68,50 +63,4 @@ func (a *Rapid) createMainWindow() fyne.Window {
 	mainWindow.SetContent(content)
 	mainWindow.Resize(fyne.NewSize(800, 600))
 	return mainWindow
-}
-
-// TODO: refactor
-func (a *Rapid) updateAddressList(list *widget.List, addressList *[]model.ServiceInstance, addresses map[string]model.ServiceInstance, ch chan model.ServiceInstance) {
-	go a.startPingLoop(list, addressList, addresses)
-
-	for addr := range ch {
-		fmt.Println("NEW ADDR: ", addr.InstanceName)
-		if v, exists := addresses[addr.IPv4]; !exists || addr.InstanceName != v.InstanceName {
-			addresses[addr.IPv4+":"+strconv.Itoa(addr.Port)] = addr
-			a.refreshAddressList(list, addressList, addresses)
-		}
-	}
-}
-
-// TODO: refactor
-func (a *Rapid) startPingLoop(list *widget.List, addressList *[]model.ServiceInstance, addresses map[string]model.ServiceInstance) {
-	ticker := time.NewTicker(1 * time.Second)
-	defer ticker.Stop()
-
-	for range ticker.C {
-		a.mu.Lock()
-		for addr, inst := range addresses {
-			port := strconv.Itoa(inst.Port)
-			fmt.Printf("Currently ping: %s:%s inst: %s\n", inst.IPv4, port, inst.InstanceName)
-			if !a.client.PingServer(addr) {
-				fmt.Println("deleted server:", inst.InstanceName)
-				delete(addresses, addr)
-			}
-		}
-		a.mu.Unlock()
-		a.refreshAddressList(list, addressList, addresses)
-	}
-}
-
-// TODO: refactor
-func (a *Rapid) refreshAddressList(list *widget.List, addressList *[]model.ServiceInstance, addresses map[string]model.ServiceInstance) {
-	*addressList = make([]model.ServiceInstance, 0, len(addresses))
-	for _, v := range addresses {
-		*addressList = append(*addressList, v)
-	}
-	list.Refresh()
-}
-
-func (a *Rapid) startScanLocalAddrs(ctx context.Context, ch chan model.ServiceInstance) {
-	a.client.DiscoverServers(ctx, ch)
 }
