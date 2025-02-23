451a9a23e4e5e729a404c96edd5e7d1a74a9afb6
0x0FACED <https.genius@gmail.com>
2025-02-23 22:00:39 +0300

    huge refactor

--- internal/rapid/controller/lan.go
+++ internal/rapid/controller/lan.go
@@ -1,185 +1,426 @@
-// TODO: impl
 package controller
 
 import (
 	"context"
 	"fmt"
+	"image/color"
 	"log"
+	"sort"
 	"strconv"
 	"sync"
 	"time"
 
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/layout"
 	"fyne.io/fyne/v2/widget"
 	"github.com/0x0FACED/rapid/internal/lan/client"
+	"github.com/0x0FACED/rapid/internal/lan/server"
 	"github.com/0x0FACED/rapid/internal/model"
 )
 
-// LANController представляет из себя структуру для
-// управления контентом списка адресов.
-// Здесь
-type LANController struct {
-	servers       map[string]model.ServiceInstance
-	serversSlice  []model.ServiceInstance
-	serversCh     chan model.ServiceInstance
-	client        *client.LANClient
-	mu            sync.Mutex
-	serversList   *widget.List
-	receivedFiles *widget.List
-	sharedFiles   *widget.List
+type ServerState struct {
+	Instances  map[string]model.ServiceInstance
+	mu         sync.RWMutex
+	sortedKeys []string
 }
 
-func NewLANController(client *client.LANClient) *LANController {
-	return &LANController{
-		servers:   make(map[string]model.ServiceInstance),
-		serversCh: make(chan model.ServiceInstance, 10),
-		client:    client,
+func NewServerState() *ServerState {
+	return &ServerState{
+		Instances: make(map[string]model.ServiceInstance),
 	}
 }
-func (c *LANController) OnLength() int {
-	return len(c.servers)
+
+func (s *ServerState) AddOrUpdate(instance model.ServiceInstance) bool {
+	key := instance.Key()
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	if _, exists := s.Instances[key]; !exists {
+		s.sortedKeys = append(s.sortedKeys, key)
+	}
+	_, ok := s.Instances[key]
+	if !ok {
+		s.Instances[key] = instance
+		return true
+	}
+
+	sort.Slice(s.sortedKeys, func(i, j int) bool {
+		return s.sortedKeys[i] < s.sortedKeys[j]
+	})
+	return false
+}
+
+func (s *ServerState) Remove(key string) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	delete(s.Instances, key)
+}
+
+func (s *ServerState) GetAll() []model.ServiceInstance {
+	s.mu.RLock()
+	defer s.mu.RUnlock()
+
+	result := make([]model.ServiceInstance, 0, len(s.sortedKeys))
+	for _, key := range s.sortedKeys {
+		if instance, exists := s.Instances[key]; exists {
+			result = append(result, instance)
+		}
+	}
+	return result
+}
+
+type FileState struct {
+	Files     map[string]model.File
+	mu        sync.RWMutex
+	sortedIDs []string
+}
+
+func NewFileState() *FileState {
+	return &FileState{
+		Files: make(map[string]model.File),
+	}
+}
+
+func (f *FileState) Add(id string, file model.File) {
+	f.mu.Lock()
+	defer f.mu.Unlock()
+
+	if _, exists := f.Files[id]; !exists {
+		f.sortedIDs = append(f.sortedIDs, id)
+		sort.Slice(f.sortedIDs, func(i, j int) bool {
+			return f.sortedIDs[i] < f.sortedIDs[j]
+		})
+	}
+	f.Files[id] = file
+}
+
+func (f *FileState) GetAll() []model.File {
+	f.mu.RLock()
+	defer f.mu.RUnlock()
+
+	result := make([]model.File, 0, len(f.sortedIDs))
+	for _, id := range f.sortedIDs {
+		if file, exists := f.Files[id]; exists {
+			result = append(result, file)
+		}
+	}
+	return result
+}
+
+type LANController struct {
+	client        *client.LANClient
+	server        *server.LANServer
+	serverState   *ServerState
+	receivedFiles *FileState
+	sharedFiles   *FileState
+	serversList   *widget.List
+	receivedList  *widget.List
+	sharedList    *widget.List
+	serversChan   chan model.ServiceInstance
+	currentServer string
+	refreshTicker *time.Ticker
+	shutdownChan  chan struct{}
+}
+
+func NewLANController(client *client.LANClient, server *server.LANServer) *LANController {
+	return &LANController{
+		client:        client,
+		server:        server,
+		serverState:   NewServerState(),
+		receivedFiles: NewFileState(),
+		sharedFiles:   NewFileState(),
+		serversChan:   make(chan model.ServiceInstance, 20),
+		shutdownChan:  make(chan struct{}),
+	}
 }
 
 func (lc *LANController) Start(ctx context.Context) {
-	go lc.startScanLocalAddrs(ctx)
-	go lc.updateAddressList(ctx)
+	go lc.startServerDiscovery(ctx)
+	go lc.startServerMaintenance(ctx)
+	go lc.processServerUpdates()
 }
 
-func (lc *LANController) startScanLocalAddrs(ctx context.Context) {
-	lc.client.DiscoverServers(ctx, lc.serversCh)
+func (lc *LANController) Stop() {
+	close(lc.shutdownChan)
+	if lc.refreshTicker != nil {
+		lc.refreshTicker.Stop()
+	}
+	close(lc.serversChan)
 }
 
-func (lc *LANController) updateAddressList(ctx context.Context) {
-	go lc.startPingLoop(ctx)
+func (lc *LANController) startServerDiscovery(ctx context.Context) {
+	lc.client.DiscoverServers(ctx, lc.serversChan)
+}
 
-	for addr := range lc.serversCh {
-		fmt.Println("NEW ADDR: ", addr.InstanceName)
-		if v, exists := lc.servers[addr.IPv4]; !exists || addr.InstanceName != v.InstanceName {
-			lc.servers[addr.IPv4+":"+strconv.Itoa(addr.Port)] = addr
-			lc.refreshAddressList()
+func (lc *LANController) startServerMaintenance(ctx context.Context) {
+	lc.refreshTicker = time.NewTicker(1 * time.Second)
+	defer lc.refreshTicker.Stop()
+
+	for {
+		select {
+		case <-lc.refreshTicker.C:
+			lc.checkServersAvailability()
+		case <-lc.shutdownChan:
+			return
+		case <-ctx.Done():
+			return
 		}
 	}
 }
 
-func (lc *LANController) startPingLoop(ctx context.Context) {
-	ticker := time.NewTicker(1 * time.Second)
-	defer ticker.Stop()
-
-	for range ticker.C {
-		lc.mu.Lock()
-		for addr, inst := range lc.servers {
-			port := strconv.Itoa(inst.Port)
-			fmt.Printf("Currently ping: %s:%s inst: %s\n", inst.IPv4, port, inst.InstanceName)
-			if !lc.client.PingServer(addr) {
-				fmt.Println("deleted server:", inst.InstanceName)
-				delete(lc.servers, addr)
-			}
+func (lc *LANController) processServerUpdates() {
+	for instance := range lc.serversChan {
+		fmt.Println("New Server!")
+		isAdded := lc.serverState.AddOrUpdate(instance)
+		if isAdded {
+			lc.refreshUI()
 		}
-		lc.mu.Unlock()
-		lc.refreshAddressList()
 	}
 }
 
-func (lc *LANController) refreshAddressList() {
-	lc.serversSlice = make([]model.ServiceInstance, 0, len(lc.servers))
-	for _, v := range lc.servers {
-		lc.serversSlice = append(lc.serversSlice, v)
+func (lc *LANController) checkServersAvailability() {
+	servers := lc.serverState.GetAll()
+	for _, server := range servers {
+		fmt.Println("Currently pinging: ", server.Address())
+		if !lc.client.PingServer(server.Address()) {
+			lc.serverState.Remove(server.Key())
+			lc.refreshUI()
+		}
 	}
+}
+
+func (lc *LANController) refreshUI() {
 	if lc.serversList != nil {
 		lc.serversList.Refresh()
+	}
+	if lc.receivedList != nil {
+		lc.receivedList.Refresh()
+	}
+	if lc.sharedList != nil {
+		lc.sharedList.Refresh()
 	}
 }
 
 func (lc *LANController) CreateLANContent() fyne.CanvasObject {
-	// Addresses list
-	addressesLabel := widget.NewLabel("Addresses")
-	addressesLabel.TextStyle = fyne.TextStyle{Bold: true, Symbol: true}
-	addressesLabel.Alignment = fyne.TextAlignCenter
+	lc.initServerList()
+	lc.initReceivedFilesList()
+	lc.initSharedFilesList()
 
+	return container.NewHSplit(
+		lc.createServerListSection(),
+		container.NewVSplit(
+			lc.createReceivedFilesSection(),
+			lc.createSharedFilesSection(),
+		),
+	)
+}
+
+func (lc *LANController) initServerList() {
 	lc.serversList = widget.NewList(
-		func() int { return len(lc.serversSlice) },
+		func() int { return len(lc.serverState.GetAll()) },
 		func() fyne.CanvasObject {
-			return container.NewHBox(
-				widget.NewLabelWithStyle("", fyne.TextAlignCenter, fyne.TextStyle{}),
+			return container.NewBorder(
+				nil,
+				nil,
 				widget.NewLabel(""),
 				widget.NewLabel(""),
+				nil,
 			)
 		},
 		func(i widget.ListItemID, o fyne.CanvasObject) {
+			servers := lc.serverState.GetAll()
+			if i >= len(servers) {
+				return
+			}
+			server := servers[i]
 			container := o.(*fyne.Container)
 			labels := container.Objects
-			labels[0].(*widget.Label).SetText(fmt.Sprintf("%d", i+1))
-			labels[1].(*widget.Label).SetText(lc.serversSlice[i].InstanceName)
-			labels[2].(*widget.Label).SetText(lc.serversSlice[i].IPv4 + ":" + strconv.Itoa(lc.serversSlice[i].Port))
+			labels[0].(*widget.Label).SetText(server.InstanceName)
+			labels[1].(*widget.Label).SetText(server.Address())
 		},
 	)
 
-	lc.serversList.MinSize()
-
-	// Received files list
-	receivedFilesLabel := widget.NewLabel("Files from address")
-	receivedFilesLabel.TextStyle = fyne.TextStyle{Bold: true, Symbol: true}
-	receivedFilesLabel.Alignment = fyne.TextAlignCenter
-	lc.receivedFiles = widget.NewList(
-		func() int {
-			return 0
-		},
-		func() fyne.CanvasObject {
-			return widget.NewLabel("File")
-		},
-		func(i widget.ListItemID, o fyne.CanvasObject) {
-			o.(*widget.Label).SetText("File info " + strconv.Itoa(i))
-		},
-	)
-
-	// Обработка нажатия на адрес
 	lc.serversList.OnSelected = func(id widget.ListItemID) {
-		addr := lc.serversSlice[id].IPv4
-		port := strconv.Itoa(lc.serversSlice[id].Port)
-		files, err := lc.client.GetFiles(addr, port)
-		if err != nil {
-			log.Println("Error getting files:", err)
+		servers := lc.serverState.GetAll()
+		if id >= len(servers) {
 			return
 		}
+		server := servers[id]
+		lc.currentServer = server.Key()
+		lc.updateReceivedFiles(server)
+		lc.serversList.Unselect(id)
+	}
+	lc.serversList.HideSeparators = true
+}
 
-		lc.receivedFiles.Length = func() int {
-			return len(files)
-		}
-		lc.receivedFiles.CreateItem = func() fyne.CanvasObject {
-			return widget.NewLabel("File")
-		}
-		lc.receivedFiles.UpdateItem = func(i widget.ListItemID, o fyne.CanvasObject) {
-			o.(*widget.Label).SetText(files[i].Name)
-		}
-		lc.receivedFiles.Refresh()
-		lc.serversList.Hide()
-		lc.serversList.Show()
+func (lc *LANController) updateReceivedFiles(server model.ServiceInstance) {
+	files, err := lc.client.GetFiles(server.IPv4, strconv.Itoa(server.Port))
+	if err != nil {
+		log.Printf("Error getting files from %s: %v", server.Address(), err)
+		return
 	}
 
-	// Shared files list
-	sharedFilesLabel := widget.NewLabel("Your shared files")
-	sharedFilesLabel.TextStyle = fyne.TextStyle{Bold: true, Symbol: true}
-	sharedFilesLabel.Alignment = fyne.TextAlignCenter
-	lc.sharedFiles = widget.NewList(
-		func() int {
-			return 0
-		},
+	lc.receivedFiles = NewFileState()
+	for _, file := range files {
+		lc.receivedFiles.Add(file.ID, file)
+	}
+	lc.receivedList.Refresh()
+}
+
+func (lc *LANController) initReceivedFilesList() {
+	lc.receivedList = widget.NewList(
+		func() int { return len(lc.receivedFiles.GetAll()) },
 		func() fyne.CanvasObject {
-			return widget.NewLabel("Shared File")
+			return container.NewBorder(
+				nil,
+				nil,
+				widget.NewLabel(""),
+				widget.NewLabel(""),
+				nil,
+			)
 		},
 		func(i widget.ListItemID, o fyne.CanvasObject) {
-			o.(*widget.Label).SetText("Shared file " + strconv.Itoa(i))
+			files := lc.receivedFiles.GetAll()
+			if i >= len(files) {
+				return
+			}
+			file := files[i]
+			container := o.(*fyne.Container)
+			labels := container.Objects
+			labels[0].(*widget.Label).SetText(file.Name)
+			labels[1].(*widget.Label).SetText(file.SizeString())
 		},
 	)
 
-	lists := container.NewHSplit(
-		container.NewBorder(addressesLabel, nil, nil, nil, lc.serversList),
-		container.NewVSplit(
-			container.NewBorder(receivedFilesLabel, nil, nil, nil, lc.receivedFiles),
-			container.NewBorder(sharedFilesLabel, nil, nil, nil, lc.sharedFiles),
-		),
+	lc.receivedList.OnSelected = func(id widget.ListItemID) {
+		files := lc.receivedFiles.GetAll()
+		if id >= len(files) {
+			return
+		}
+		file := files[id]
+		lc.downloadFile(file)
+		lc.receivedList.Unselect(id)
+	}
+
+	lc.receivedList.HideSeparators = true
+}
+
+func (lc *LANController) downloadFile(file model.File) {
+	servers := lc.serverState.GetAll()
+	if lc.currentServer == "" || len(servers) == 0 {
+		return
+	}
+
+	server := lc.findCurrentServer()
+	if server == nil {
+		return
+	}
+
+	err := lc.client.DownloadFile(
+		server.IPv4,
+		strconv.Itoa(server.Port),
+		file.ID,
+		file.Name,
+	)
+	if err != nil {
+		log.Printf("Error downloading file %s: %v", file.Name, err)
+	}
+}
+
+func (lc *LANController) findCurrentServer() *model.ServiceInstance {
+	servers := lc.serverState.GetAll()
+	for _, server := range servers {
+		if server.Key() == lc.currentServer {
+			return &server
+		}
+	}
+	return nil
+}
+
+func (lc *LANController) initSharedFilesList() {
+	lc.sharedList = widget.NewList(
+		func() int { return len(lc.sharedFiles.GetAll()) },
+		func() fyne.CanvasObject {
+			return container.NewBorder(
+				nil,
+				nil,
+				widget.NewLabel(""),
+				widget.NewLabel(""),
+				nil,
+			)
+		},
+		func(i widget.ListItemID, o fyne.CanvasObject) {
+			files := lc.sharedFiles.GetAll()
+			if i >= len(files) {
+				return
+			}
+			file := files[i]
+			container := o.(*fyne.Container)
+			labels := container.Objects
+			labels[0].(*widget.Label).SetText(file.Name)
+			labels[1].(*widget.Label).SetText(file.SizeString())
+		},
+	)
+	lc.sharedList.HideSeparators = true
+}
+
+func (lc *LANController) createServerListSection() fyne.CanvasObject {
+	spacer := layout.NewSpacer()
+
+	header := container.NewGridWithColumns(3,
+		widget.NewLabelWithStyle("Instance Name", fyne.TextAlignLeading, fyne.TextStyle{Bold: true}),
+		spacer,
+		widget.NewLabelWithStyle("Address", fyne.TextAlignTrailing, fyne.TextStyle{Bold: true}),
+	)
+	label := widget.NewLabelWithStyle("Local Addresses", fyne.TextAlignCenter, fyne.TextStyle{Bold: true})
+
+	separator := NewCustomSeparator(
+		color.RGBA{R: 200, G: 200, B: 200, A: 255},
+		1,
+		true,
 	)
 
-	return lists
+	cont := container.NewVBox(label, header, separator)
+	return container.NewBorder(cont, nil, nil, nil, lc.serversList)
+}
+
+func (lc *LANController) createReceivedFilesSection() fyne.CanvasObject {
+	spacer := layout.NewSpacer()
+
+	header := container.NewGridWithColumns(3,
+		widget.NewLabelWithStyle("Filename", fyne.TextAlignLeading, fyne.TextStyle{Bold: true}),
+		spacer,
+		widget.NewLabelWithStyle("Size", fyne.TextAlignTrailing, fyne.TextStyle{Bold: true}),
+	)
+	label := widget.NewLabelWithStyle("Received Files", fyne.TextAlignCenter, fyne.TextStyle{Bold: true})
+
+	separator := NewCustomSeparator(
+		color.RGBA{R: 200, G: 200, B: 200, A: 255},
+		1,
+		true,
+	)
+
+	cont := container.NewBorder(label, header, nil, nil, separator)
+	return container.NewBorder(cont, nil, nil, nil, lc.receivedList)
+}
+
+func (lc *LANController) createSharedFilesSection() fyne.CanvasObject {
+	spacer := layout.NewSpacer()
+
+	header := container.NewGridWithColumns(3,
+		widget.NewLabelWithStyle("Filename", fyne.TextAlignLeading, fyne.TextStyle{Bold: true}),
+		spacer,
+		widget.NewLabelWithStyle("Size", fyne.TextAlignTrailing, fyne.TextStyle{Bold: true}),
+	)
+	label := widget.NewLabelWithStyle("Your Shared files", fyne.TextAlignCenter, fyne.TextStyle{Bold: true})
+
+	separator := NewCustomSeparator(
+		color.RGBA{R: 200, G: 200, B: 200, A: 255},
+		1,
+		true,
+	)
+
+	cont := container.NewBorder(label, header, nil, nil, separator)
+	return container.NewBorder(cont, nil, nil, nil, lc.sharedList)
 }
